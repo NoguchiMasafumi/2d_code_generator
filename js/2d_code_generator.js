edf5ea5b76cad9ffadadae3e607a01028af734e8
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2021-09-12 17:31:41 +0900

    Update 2d_code_generator.js

--- js/2d_code_generator.js
+++ js/2d_code_generator.js
@@ -1,11 +1,11 @@
 			//*********
 			document.getElementById("code").focus();
 			document.getElementById("but").addEventListener("click", async({target})=>{
-				document.getElementById("canvas").getContext("bitmaprenderer").transferFromImageBitmap((await 2d_code_generate(document.getElementById("code").value)).transferToImageBitmap());
+				document.getElementById("canvas").getContext("bitmaprenderer").transferFromImageBitmap((await two_dimention_code_generate(document.getElementById("code").value)).transferToImageBitmap());
 			});
 
 			//*********
-			function 2d_code_generate(data){
+			function two_dimention_code_generate(data){
 				const canvas = new OffscreenCanvas(1, 1);
 				$("#canvas").show()
 				return new Promise((res, rej) => QRCode.toCanvas(canvas, data, {}, err => !err ? res(canvas) : rej(err)));
